ab637b100a734e2f42d91a455e5feb0927c9f43d
Edlâneo Júlio <edlanioj@gmail.com>
2022-04-18 07:57:41 +0100

    update server port to env

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, () => {
+    console.log(`App is running on port: ${PORT}`);
+  });
 }
 bootstrap();
