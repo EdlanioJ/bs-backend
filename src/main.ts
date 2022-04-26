3c05b542425bb42e4b6a7d268a3907ee342a1ff9
Edlâneo Júlio <edlanioj@gmail.com>
2022-04-26 21:26:16 +0100

    feat: add swagger initial config

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config: ConfigService = app.get(ConfigService);
-  const port = config.get<number>('PORT');
+  const configService: ConfigService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Beauty Spaces API')
+    .setVersion('0.0.1')
+    .setDescription('API for Beauty Spaces')
+    .setExternalDoc('GitHub', 'https://github.com/EdlanioJ/bs-backend')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port, () => {
     console.log(`App is running on port: ${port}`);
   });
