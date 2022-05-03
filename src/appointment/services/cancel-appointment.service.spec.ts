731e62801accc2853d86324ff4f6aa274e778bd3
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-03 17:58:22 +0100

    test: ensure throw if  findOne appointmnet returns nul

--- src/appointment/services/cancel-appointment.service.spec.ts
+++ src/appointment/services/cancel-appointment.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-
+import { UnauthorizedException } from '@nestjs/common';
 import { CancelAppointmentService } from './cancel-appointment.service';
 import { AppointmentRepository } from '../repositories';
 
-jest.mock('../repositories');
+jest.mock('../repositories', () =>
+  jest.requireActual('../../../test/mocks/repositories/appointment.mock'),
+);
 
 describe('CancelAppointmentService', () => {
   let service: CancelAppointmentService;
+  let appointmentRepo: AppointmentRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,9 +17,26 @@ describe('CancelAppointmentService', () => {
     }).compile();
 
     service = module.get<CancelAppointmentService>(CancelAppointmentService);
+    appointmentRepo = module.get<AppointmentRepository>(AppointmentRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  const input = {
+    appointmentId: 'any_appointment_id',
+    reason: 'any_reason',
+    userId: 'any_user_id',
+  };
+  it('should throw if appointment repository findOne return null', () => {
+    const spy = jest.spyOn(appointmentRepo, 'findOne').mockResolvedValue(null);
+    const output = service.execute(input);
+
+    expect(spy).toBeCalled();
+    expect(spy).toBeCalledWith(input.appointmentId);
+    expect(output).rejects.toThrowError(
+      new UnauthorizedException('Appointment not found'),
+    );
   });
 });
