dc5b3751d2963ecf992173a18e016c67fd697321
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-03 21:01:25 +0100

    test: ensure CompleteAppointmentService.execute throw UnauthorizedException if appointment repo findOne method returns null

--- src/appointment/services/complete-appointment.service.spec.ts
+++ src/appointment/services/complete-appointment.service.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CompleteAppointmentService } from './complete-appointment.service';
 import { AppointmentRepository } from '../repositories';
+import { UnauthorizedException } from '@nestjs/common';
 
-jest.mock('../repositories');
+jest.mock('../repositories', () =>
+  jest.requireActual('../../../test/mocks/repositories/appointment.mock'),
+);
 
 describe('CompleteAppointmentService', () => {
   let service: CompleteAppointmentService;
+  let appointmentRepo: AppointmentRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,9 +19,22 @@ describe('CompleteAppointmentService', () => {
     service = module.get<CompleteAppointmentService>(
       CompleteAppointmentService,
     );
+    appointmentRepo = module.get<AppointmentRepository>(AppointmentRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  const input = { appointmentId: 'any_appointment_id', userId: 'any_user_id' };
+
+  it('should throw UnauthorizedException if appointment repo findOne method returns null', () => {
+    const spy = jest.spyOn(appointmentRepo, 'findOne').mockResolvedValue(null);
+    const output = service.execute(input);
+    expect(spy).toBeCalledTimes(1);
+    expect(spy).toBeCalledWith(input.appointmentId);
+    expect(output).rejects.toThrowError(
+      new UnauthorizedException('Appointment not found'),
+    );
   });
 });
