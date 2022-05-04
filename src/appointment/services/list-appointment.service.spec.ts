28b80456e99b03b205382ae53fe20c0e571ebaa9
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-04 08:32:11 +0100

    ensure ListAppointmentService return an array of appointments

--- src/appointment/services/list-appointment.service.spec.ts
+++ src/appointment/services/list-appointment.service.spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { appointmentStub } from '../../../test/mocks/stubs';
 import { AppointmentRepository } from '../repositories';
 import { ListAppointmentService } from './list-appointment.service';
 
 jest.mock('../repositories');
 describe('ListAppointmentService', () => {
   let service: ListAppointmentService;
+  let appointmentRepo: AppointmentRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +14,27 @@ describe('ListAppointmentService', () => {
     }).compile();
 
     service = module.get<ListAppointmentService>(ListAppointmentService);
+    appointmentRepo = module.get<AppointmentRepository>(AppointmentRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return an array of appointments', async () => {
+    const stub = appointmentStub();
+    const result = [stub];
+    const findAllSpy = jest
+      .spyOn(appointmentRepo, 'findAll')
+      .mockResolvedValue(result);
+    const countSpy = jest.spyOn(appointmentRepo, 'count').mockResolvedValue(1);
+    const { data, total } = await service.execute({
+      page: 1,
+      limit: 1,
+    });
+    expect(total).toBe(1);
+    expect(data[0]).toEqual(expect.objectContaining({ id: stub.id }));
+    expect(findAllSpy).toHaveBeenCalledTimes(1);
+    expect(countSpy).toHaveBeenCalledTimes(1);
   });
 });
