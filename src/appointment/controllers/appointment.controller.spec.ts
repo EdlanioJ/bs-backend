81491a2d7ba99ad0fa3f414b95553e466d8b3b83
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-04 13:52:10 +0100

    ensure AppointmentController create appointment

--- src/appointment/controllers/appointment.controller.spec.ts
+++ src/appointment/controllers/appointment.controller.spec.ts
@@ -14,7 +14,7 @@ jest.mock('../services');
 
 describe('AppointmentController', () => {
   let controller: AppointmentController;
-
+  let createAppointment: CreateAppointmentService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AppointmentController],
@@ -30,9 +30,35 @@ describe('AppointmentController', () => {
     }).compile();
 
     controller = module.get<AppointmentController>(AppointmentController);
+    createAppointment = module.get<CreateAppointmentService>(
+      CreateAppointmentService,
+    );
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create appointment', async () => {
+    const spy = jest.spyOn(createAppointment, 'execute');
+    const employeeId = 'employee_id';
+    const customerId = 'customer_id';
+    const serviceId = 'service_id';
+    const start = new Date();
+    await controller.create(
+      {
+        employeeId,
+        serviceId,
+        startAt: start,
+      },
+      customerId,
+    );
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith({
+      employeeId,
+      serviceId,
+      customerId,
+      startTime: start,
+    });
   });
 });
