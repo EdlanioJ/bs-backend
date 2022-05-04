e05642acbb3d7e7acc033f2d185350e301d2db30
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-04 09:45:34 +0100

    ensure AppointmentRepository find findAvailable

--- src/appointment/repositories/appointment.repository.spec.ts
+++ src/appointment/repositories/appointment.repository.spec.ts
@@ -1,11 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { appointmentStub } from '../../../test/mocks/stubs';
 import { PrismaService } from '../../prisma';
 import { AppointmentRepository } from './appointment.repository';
 
-jest.mock('../../prisma');
-
 describe('AppointmentRepository', () => {
   let repository: AppointmentRepository;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,9 +13,20 @@ describe('AppointmentRepository', () => {
     }).compile();
 
     repository = module.get<AppointmentRepository>(AppointmentRepository);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(repository).toBeDefined();
+  });
+
+  it('should find findAvailable', async () => {
+    const employeeId = 'employee_id';
+    const start = new Date();
+    const end = new Date();
+    const appointment = appointmentStub();
+    prisma.appointment.findFirst = jest.fn().mockResolvedValueOnce(appointment);
+    const result = await repository.findAvailable(employeeId, start, end);
+    expect(result).toBe(appointment);
   });
 });
