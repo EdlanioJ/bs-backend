68779e880acdc608318ca3028b7e3d1c8c890750
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-04 01:48:17 +0100

    refactor:  user stab property

--- test/mocks/stubs/service.stub.ts
+++ test/mocks/stubs/service.stub.ts
@@ -2,7 +2,7 @@ import faker from '@faker-js/faker';
 import { Service } from '@prisma/client';
 
 export const serviceStub = (): Service => ({
-  appointmentDurationInMinutes: faker.random.number(),
+  appointmentDurationInMinutes: faker.datatype.number({ min: 1, max: 60 }),
   id: 'any_id',
   name: faker.name.jobArea(),
   createdAt: faker.date.past(),
