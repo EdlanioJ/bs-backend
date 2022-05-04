a8239292eb69a2eb2ed306373104177e01b96db7
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-04 06:24:16 +0100

    ensure GetAppointmentService return an appointment

--- src/appointment/services/get-appointment.service.spec.ts
+++ src/appointment/services/get-appointment.service.spec.ts
@@ -1,11 +1,13 @@
 import { TestingModule, Test } from '@nestjs/testing';
 import { GetAppointmentService } from './get-appointment.service';
 import { AppointmentRepository } from '../repositories';
+import { appointmentStub } from '../../../test/mocks/stubs';
 
 jest.mock('../repositories');
 
 describe('GetAppointmentService', () => {
   let service: GetAppointmentService;
+  let appointmentRepo: AppointmentRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,9 +15,22 @@ describe('GetAppointmentService', () => {
     }).compile();
 
     service = module.get<GetAppointmentService>(GetAppointmentService);
+    appointmentRepo = module.get<AppointmentRepository>(AppointmentRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return an appointment', async () => {
+    const input = { id: 'any_id' };
+    const response = appointmentStub();
+    const spy = jest
+      .spyOn(appointmentRepo, 'findOne')
+      .mockResolvedValue(response);
+
+    const output = await service.execute(input);
+    expect(spy).toHaveBeenCalledWith(input.id);
+    expect(output).toEqual(expect.objectContaining({ id: response.id }));
   });
 });
