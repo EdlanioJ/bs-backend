cc5c7e11c251498a14fc9d2c74ec3fb9c05f9156
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-06 09:35:45 +0100

    test: ensure execute throw BadRequestException if userRepo.findOneByEmail return null

--- src/auth/services/forgot-password.service.spec.ts
+++ src/auth/services/forgot-password.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { SendMailProducerService } from '../../mail/services';
 import { UserRepository } from '../../user/repositories';
@@ -8,6 +9,7 @@ jest.mock('../../user/repositories');
 
 describe('ForgotPasswordService', () => {
   let service: ForgotPasswordService;
+  let userRepo: UserRepository;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -19,9 +21,21 @@ describe('ForgotPasswordService', () => {
     }).compile();
 
     service = module.get<ForgotPasswordService>(ForgotPasswordService);
+    userRepo = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should throw BadRequestException if userRepo.findOneByEmail return null', async () => {
+    const email = 'any_email';
+    const spy = jest
+      .spyOn(userRepo, 'findOneByEmail')
+      .mockResolvedValueOnce(null);
+    const output = service.execute({ email });
+
+    expect(output).rejects.toThrow(new BadRequestException('User not found'));
+    expect(spy).toHaveBeenCalledWith(email);
   });
 });
