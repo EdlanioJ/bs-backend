e79b48b4c64d2a0f08b75ace00b6b8cbc5d752ce
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-06 10:03:00 +0100

    test: ensure execute call mailProducer.execute with correct params

--- src/auth/services/forgot-password.service.spec.ts
+++ src/auth/services/forgot-password.service.spec.ts
@@ -11,6 +11,7 @@ jest.mock('../../user/repositories');
 describe('ForgotPasswordService', () => {
   let service: ForgotPasswordService;
   let userRepo: UserRepository;
+  let mailProducer: SendMailProducerService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -23,6 +24,7 @@ describe('ForgotPasswordService', () => {
 
     service = module.get<ForgotPasswordService>(ForgotPasswordService);
     userRepo = module.get<UserRepository>(UserRepository);
+    mailProducer = module.get<SendMailProducerService>(SendMailProducerService);
   });
 
   it('should be defined', () => {
@@ -52,6 +54,36 @@ describe('ForgotPasswordService', () => {
     expect(updateSpy).toHaveBeenCalledWith(user.id, {
       resetPasswordExpires: expect.any(Date),
       resetPasswordToken: expect.any(String),
+    });
+  });
+
+  it('should call mailProducer.execute with correct params', async () => {
+    const email = 'any_email';
+    const user = userStub();
+    const findSpy = jest
+      .spyOn(userRepo, 'findOneByEmail')
+      .mockResolvedValueOnce(user);
+    const updateSpy = jest.spyOn(userRepo, 'update');
+    const mailSpy = jest.spyOn(mailProducer, 'execute');
+    await service.execute({ email });
+    expect(findSpy).toHaveBeenCalledWith(email);
+    expect(updateSpy).toHaveBeenCalledWith(user.id, {
+      resetPasswordExpires: expect.any(Date),
+      resetPasswordToken: expect.any(String),
+    });
+    expect(mailSpy).toHaveBeenCalledWith({
+      to: user.email,
+      type: 'forgot-password',
+      content: [
+        {
+          key: 'name',
+          value: user.name,
+        },
+        {
+          key: 'token',
+          value: expect.any(String),
+        },
+      ],
     });
   });
 });
