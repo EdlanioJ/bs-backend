7186b8c4728e27ffeb04039362572aa157b537d9
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-07 20:10:29 +0100

    test: ensure ResetPasswordService.execute update user password

--- src/auth/services/reset-password.service.spec.ts
+++ src/auth/services/reset-password.service.spec.ts
@@ -13,8 +13,10 @@ jest.mock('../../mail/services');
 jest.mock('../helpers');
 
 describe('ResetPasswordService', () => {
+  let mailProducer: SendMailProducerService;
   let service: ResetPasswordService;
   let userRepo: UserRepository;
+  let authHelpers: AuthHelpers;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -28,6 +30,8 @@ describe('ResetPasswordService', () => {
 
     service = module.get<ResetPasswordService>(ResetPasswordService);
     userRepo = module.get<UserRepository>(UserRepository);
+    mailProducer = module.get<SendMailProducerService>(SendMailProducerService);
+    authHelpers = module.get<AuthHelpers>(AuthHelpers);
   });
 
   it('should be defined', () => {
@@ -63,5 +67,40 @@ describe('ResetPasswordService', () => {
     await expect(out).rejects.toThrowError(
       new BadRequestException('Token expired'),
     );
+  });
+
+  it('should update user password', async () => {
+    const user = { ...userStub(), resetPasswordExpires: faker.date.future() };
+    const findTokenSpy = jest
+      .spyOn(userRepo, 'findOneByResetToken')
+      .mockResolvedValueOnce(user);
+    const hashSpy = jest
+      .spyOn(authHelpers, 'hashData')
+      .mockResolvedValueOnce('hashedPassword');
+    const updateSpy = jest.spyOn(userRepo, 'update');
+    const mailSpy = jest.spyOn(mailProducer, 'execute');
+
+    const token = 'token';
+    const password = 'password';
+
+    await service.execute({ token, password });
+
+    expect(findTokenSpy).toHaveBeenCalledWith(token);
+    expect(hashSpy).toHaveBeenCalledWith(password);
+    expect(updateSpy).toHaveBeenCalledWith(user.id, {
+      password: 'hashedPassword',
+      resetPasswordToken: null,
+      resetPasswordExpires: null,
+    });
+    expect(mailSpy).toHaveBeenCalledWith({
+      to: user.email,
+      type: 'reset-password-success',
+      content: [
+        {
+          key: 'name',
+          value: user.name,
+        },
+      ],
+    });
   });
 });
