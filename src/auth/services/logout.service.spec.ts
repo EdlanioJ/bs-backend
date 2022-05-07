074089635847c17e47a919d2558cc15d756af81e
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-07 08:48:00 +0100

    test: ensure execute call userRepo.update with correct params

--- src/auth/services/logout.service.spec.ts
+++ src/auth/services/logout.service.spec.ts
@@ -6,6 +6,7 @@ jest.mock('../../user/repositories');
 
 describe('LogoutService', () => {
   let service: LogoutService;
+  let userRepo: UserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,9 +14,18 @@ describe('LogoutService', () => {
     }).compile();
 
     service = module.get<LogoutService>(LogoutService);
+    userRepo = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should call userRepo.update with correct params', async () => {
+    const userId = 'userId';
+    const spy = jest.spyOn(userRepo, 'update');
+    await service.execute({ userId });
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(userId, { refreshToken: null });
   });
 });
