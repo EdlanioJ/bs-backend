70d67b46028d1ca1e7f0c10e6cad2944e8b54802
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-07 20:58:53 +0100

    test: ensure ValidateOAuthService.execute update user

--- src/auth/services/validate-oauth.service.spec.ts
+++ src/auth/services/validate-oauth.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { userStub } from '../../../test/mocks/stubs';
 import { SendMailProducerService } from '../../mail/services';
 import { UserRepository } from '../../user/repositories';
 import { ValidateOAuthService } from './validate-oauth.service';
@@ -8,6 +9,7 @@ jest.mock('../../user/repositories');
 
 describe('ValidateOAuthService', () => {
   let service: ValidateOAuthService;
+  let userRepo: UserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,9 +21,44 @@ describe('ValidateOAuthService', () => {
     }).compile();
 
     service = module.get<ValidateOAuthService>(ValidateOAuthService);
+    userRepo = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should update user', async () => {
+    const user = userStub();
+    const findSpy = jest
+      .spyOn(userRepo, 'findOneByThirdPartyId')
+      .mockResolvedValueOnce(user);
+    const updateSpy = jest
+      .spyOn(userRepo, 'update')
+      .mockResolvedValueOnce(user);
+
+    const result = await service.execute({
+      avatar: user.avatar,
+      email: user.email,
+      name: user.name,
+      provider: user.provider,
+      thirdPartyId: user.thirdPartyId,
+    });
+
+    expect(result).toEqual({
+      id: user.id,
+      username: user.name,
+      role: user.role,
+    });
+    expect(findSpy).toHaveBeenCalledTimes(1);
+    expect(findSpy).toHaveBeenCalledWith(user.thirdPartyId);
+    expect(updateSpy).toHaveBeenCalledTimes(1);
+    expect(updateSpy).toHaveBeenCalledWith(user.id, {
+      avatar: user.avatar,
+      email: user.email,
+      name: user.name,
+      provider: user.provider,
+      thirdPartyId: user.thirdPartyId,
+    });
   });
 });
