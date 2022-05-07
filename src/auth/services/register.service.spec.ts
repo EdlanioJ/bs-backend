ca37759f94b20b48c8f86a40c8914e2d37dad033
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-07 17:20:26 +0100

    test: ensure execute throw BadRequestException if user already exists

--- src/auth/services/register.service.spec.ts
+++ src/auth/services/register.service.spec.ts
@@ -1,4 +1,6 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { userStub } from '../../../test/mocks/stubs';
 import { SendMailProducerService } from '../../mail/services';
 import { UserRepository } from '../../user/repositories';
 import { AuthHelpers } from '../helpers';
@@ -10,6 +12,7 @@ jest.mock('../helpers');
 
 describe('RegisterService', () => {
   let service: RegisterService;
+  let userRepo: UserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,9 +25,28 @@ describe('RegisterService', () => {
     }).compile();
 
     service = module.get<RegisterService>(RegisterService);
+    userRepo = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should throw BadRequestException if user already exists', async () => {
+    const email = 'any_email';
+    const name = 'any_name';
+    const password = 'any_password';
+    const user = userStub();
+    user.email = email;
+
+    const spy = jest
+      .spyOn(userRepo, 'findOneByEmail')
+      .mockResolvedValueOnce(user);
+
+    const output = service.execute({ email, name, password });
+    await expect(output).rejects.toThrowError(
+      new BadRequestException('User already exists'),
+    );
+    expect(spy).toHaveBeenCalledWith(email);
   });
 });
