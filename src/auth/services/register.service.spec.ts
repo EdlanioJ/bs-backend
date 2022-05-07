e29eb32b4d815d13339cf3f62c98ba66fed5a0d3
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-07 17:28:20 +0100

    test: ensure execute create a new user

--- src/auth/services/register.service.spec.ts
+++ src/auth/services/register.service.spec.ts
@@ -13,6 +13,8 @@ jest.mock('../helpers');
 describe('RegisterService', () => {
   let service: RegisterService;
   let userRepo: UserRepository;
+  let mailProducer: SendMailProducerService;
+  let authHelpers: AuthHelpers;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -26,6 +28,8 @@ describe('RegisterService', () => {
 
     service = module.get<RegisterService>(RegisterService);
     userRepo = module.get<UserRepository>(UserRepository);
+    mailProducer = module.get<SendMailProducerService>(SendMailProducerService);
+    authHelpers = module.get<AuthHelpers>(AuthHelpers);
   });
 
   it('should be defined', () => {
@@ -48,5 +52,46 @@ describe('RegisterService', () => {
       new BadRequestException('User already exists'),
     );
     expect(spy).toHaveBeenCalledWith(email);
+  });
+
+  it('should create a new user', async () => {
+    const email = 'any_email';
+    const name = 'any_name';
+    const password = 'any_password';
+    const user = userStub();
+
+    const findSpy = jest
+      .spyOn(userRepo, 'findOneByEmail')
+      .mockResolvedValueOnce(null);
+    const createSpy = jest
+      .spyOn(userRepo, 'create')
+      .mockResolvedValueOnce(user);
+    const sendMailSpy = jest.spyOn(mailProducer, 'execute');
+    const hashSpy = jest
+      .spyOn(authHelpers, 'hashData')
+      .mockResolvedValueOnce('any_hash');
+
+    await service.execute({ email, name, password });
+    expect(findSpy).toHaveBeenCalledWith(email);
+    expect(createSpy).toHaveBeenCalledWith({
+      email,
+      name,
+      password: 'any_hash',
+      avatar: 'default.png',
+    });
+    expect(sendMailSpy).toHaveBeenCalledWith(
+      {
+        to: email,
+        type: 'welcome-email',
+        content: [
+          {
+            key: 'name',
+            value: name,
+          },
+        ],
+      },
+      { attempts: 3 },
+    );
+    expect(hashSpy).toHaveBeenCalledWith(password);
   });
 });
