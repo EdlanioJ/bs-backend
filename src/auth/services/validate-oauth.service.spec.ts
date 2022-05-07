e56dcbed5a4e7a2f2e52547946b29f81b8bef291
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-07 21:07:07 +0100

    test: ensure ValidateOAuthService.execute create user

--- src/auth/services/validate-oauth.service.spec.ts
+++ src/auth/services/validate-oauth.service.spec.ts
@@ -10,6 +10,7 @@ jest.mock('../../user/repositories');
 describe('ValidateOAuthService', () => {
   let service: ValidateOAuthService;
   let userRepo: UserRepository;
+  let mailProducer: SendMailProducerService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,6 +23,7 @@ describe('ValidateOAuthService', () => {
 
     service = module.get<ValidateOAuthService>(ValidateOAuthService);
     userRepo = module.get<UserRepository>(UserRepository);
+    mailProducer = module.get<SendMailProducerService>(SendMailProducerService);
   });
 
   it('should be defined', () => {
@@ -60,5 +62,54 @@ describe('ValidateOAuthService', () => {
       provider: user.provider,
       thirdPartyId: user.thirdPartyId,
     });
+  });
+
+  it('should create user', async () => {
+    const user = userStub();
+    const findSpy = jest
+      .spyOn(userRepo, 'findOneByThirdPartyId')
+      .mockResolvedValueOnce(null);
+    const createSpy = jest
+      .spyOn(userRepo, 'create')
+      .mockResolvedValueOnce(user);
+    const mailSpy = jest.spyOn(mailProducer, 'execute');
+
+    const result = await service.execute({
+      avatar: user.avatar,
+      email: user.email,
+      name: user.name,
+      provider: user.provider,
+      thirdPartyId: user.thirdPartyId,
+    });
+
+    expect(result).toEqual({
+      id: user.id,
+      username: user.name,
+      role: user.role,
+    });
+    expect(findSpy).toHaveBeenCalledTimes(1);
+    expect(findSpy).toHaveBeenCalledWith(user.thirdPartyId);
+    expect(createSpy).toHaveBeenCalledTimes(1);
+    expect(createSpy).toHaveBeenCalledWith({
+      avatar: user.avatar,
+      email: user.email,
+      name: user.name,
+      provider: user.provider,
+      thirdPartyId: user.thirdPartyId,
+    });
+    expect(mailSpy).toHaveBeenCalledTimes(1);
+    expect(mailSpy).toHaveBeenCalledWith(
+      {
+        to: user.email,
+        type: 'welcome-email',
+        content: [
+          {
+            key: 'name',
+            value: user.name,
+          },
+        ],
+      },
+      { attempts: 3 },
+    );
   });
 });
