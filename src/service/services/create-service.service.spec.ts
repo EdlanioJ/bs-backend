19efcc2e82184785e708f56fc15b8d3053a0c82a
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-09 00:50:25 +0100

    test: ensure CreateProviderServiceService throw BadRequestException if user has no Provider

--- src/service/services/create-service.service.spec.ts
+++ src/service/services/create-service.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { ServiceProviderRepository } from '../../service-provider/repositories';
 import { ServiceRepository } from '../repositories';
@@ -8,6 +9,7 @@ jest.mock('../repositories');
 
 describe('CreateProviderServiceService', () => {
   let service: CreateProviderServiceService;
+  let providerRepo: ServiceProviderRepository;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -20,9 +22,30 @@ describe('CreateProviderServiceService', () => {
     service = module.get<CreateProviderServiceService>(
       CreateProviderServiceService,
     );
+    providerRepo = module.get<ServiceProviderRepository>(
+      ServiceProviderRepository,
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should throw BadRequestException if user has no Provider', async () => {
+    const userId = 'userId';
+    const name = 'name';
+    const appointmentDurationInMinutes = 1;
+    const spy = jest
+      .spyOn(providerRepo, 'findByUserId')
+      .mockResolvedValue(null);
+    const out = service.execute({
+      appointmentDurationInMinutes,
+      name,
+      userId,
+    });
+    await expect(out).rejects.toThrow(
+      new BadRequestException('User has no Provider'),
+    );
+    expect(spy).toHaveBeenCalledWith(userId);
   });
 });
