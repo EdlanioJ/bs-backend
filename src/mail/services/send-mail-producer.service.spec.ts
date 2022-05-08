8cc7b55a9f598a0eed45cc737bd9d7267e113c21
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-08 22:46:56 +0100

    test: ensure SendMailProducerService call queue with correct values

--- src/mail/services/send-mail-producer.service.spec.ts
+++ src/mail/services/send-mail-producer.service.spec.ts
@@ -1,11 +1,11 @@
 import { Test } from '@nestjs/testing';
 import { getQueueToken } from '@nestjs/bull';
-import { Queue } from 'bull';
-
 import { SendMailProducerService } from './send-mail-producer.service';
+import { Queue } from 'bull';
 
 describe('SendMailProducerService', () => {
   let service: SendMailProducerService;
+  let queue: Queue;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -19,9 +19,28 @@ describe('SendMailProducerService', () => {
     }).compile();
 
     service = module.get<SendMailProducerService>(SendMailProducerService);
+    queue = module.get<Queue>(getQueueToken('send-mail-queue'));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should call queue with correct values', async () => {
+    const data = {
+      to: 'any_email',
+      type: 'any_type',
+      content: [
+        {
+          key: 'any_key',
+          value: 'any_value',
+        },
+      ],
+    };
+    const spy = jest.spyOn(queue, 'add');
+    await service.execute(data);
+
+    expect(spy).toHaveBeenCalledWith('send-mail-job', data, undefined);
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
