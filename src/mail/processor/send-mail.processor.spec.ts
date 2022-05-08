6608455b21af20a4c8815e27a40ca75f11e3c565
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-08 15:24:27 +0100

    test: ensure SendMailProcessor call send mail service with correct data

--- src/mail/processor/send-mail.processor.spec.ts
+++ src/mail/processor/send-mail.processor.spec.ts
@@ -6,6 +6,7 @@ jest.mock('../services');
 
 describe('SendMailProcessor', () => {
   let processor: SendMailProcessor;
+  let sendMailService: SendMailProcessorService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -13,9 +14,26 @@ describe('SendMailProcessor', () => {
     }).compile();
 
     processor = module.get<SendMailProcessor>(SendMailProcessor);
+    sendMailService = module.get<SendMailProcessorService>(
+      SendMailProcessorService,
+    );
   });
 
   it('should be defined', () => {
     expect(processor).toBeDefined();
+  });
+
+  it('should call send mail service with correct data', async () => {
+    const job: any = {
+      data: {
+        to: 'to_any',
+        type: 'any_type',
+        content: [{ key: 'key_any', value: 'value_any' }],
+      },
+    };
+    const spy = jest.spyOn(sendMailService, 'execute');
+    await processor.sendMail(job);
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(job.data);
   });
 });
