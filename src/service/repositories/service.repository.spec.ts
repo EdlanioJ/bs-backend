18c4bac8c0223356e94f311f308feba6c96a5d1b
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-09 09:37:00 +0100

    test: ensure ServiceRepository create new service

--- src/service/repositories/service.repository.spec.ts
+++ src/service/repositories/service.repository.spec.ts
@@ -1,18 +1,34 @@
 import { Test } from '@nestjs/testing';
+import { serviceStub } from '../../../test/mocks/stubs';
 import { PrismaService } from '../../prisma';
 import { ServiceRepository } from './service.repository';
 
 describe('ServiceRepository', () => {
   let repository: ServiceRepository;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       providers: [ServiceRepository, PrismaService],
     }).compile();
     repository = module.get<ServiceRepository>(ServiceRepository);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(repository).toBeDefined();
+  });
+
+  it('should create new service', async () => {
+    const service = serviceStub();
+    prisma.service.create = jest.fn().mockResolvedValueOnce(service);
+    const result = await repository.create({
+      id: 'service_id',
+      name: 'service_name',
+      provider: { connect: { id: 'provider_id' } },
+      createdAt: new Date(),
+      appointmentDurationInMinutes: 30,
+    });
+    expect(result).toBe(service);
   });
 });
