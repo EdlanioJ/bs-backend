662942e1e1509685cec72f150711a061ac0e19a3
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-09 22:26:48 +0100

    test: ensure RequireConnectionService throw BadRequestException if user to connect not found

--- src/service-provider/services/require-connection.service.spec.ts
+++ src/service-provider/services/require-connection.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test } from '@nestjs/testing';
+import { BadRequestException } from '@nestjs/common';
 import { RequireConnectionService } from './require-connection.service';
 import { SendMailProducerService } from '../../mail/services';
 import { UserRepository } from '../../user/repositories';
@@ -6,7 +7,7 @@ import {
   RequestConnectionRepository,
   ServiceProviderRepository,
 } from '../repositories';
-import { BadRequestException } from '@nestjs/common';
+import { serviceProviderStub } from '../../../test/mocks/stubs';
 
 jest.mock('../repositories');
 jest.mock('../../user/repositories');
@@ -15,6 +16,7 @@ jest.mock('../../mail/services');
 describe('RequireConnectionService', () => {
   let service: RequireConnectionService;
   let providerRepo: ServiceProviderRepository;
+  let userRepo: UserRepository;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -31,6 +33,7 @@ describe('RequireConnectionService', () => {
     providerRepo = module.get<ServiceProviderRepository>(
       ServiceProviderRepository,
     );
+    userRepo = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
@@ -48,5 +51,18 @@ describe('RequireConnectionService', () => {
       new BadRequestException('Provider not found'),
     );
     expect(spy).toHaveBeenCalledWith(providerOwnerId);
+  });
+
+  it('should throw BadRequestException if user to connect not found', async () => {
+    const providerOwnerId = 'providerOwnerId';
+    const userToConnectId = 'userToConnectId';
+    const provider = serviceProviderStub();
+    jest.spyOn(providerRepo, 'findByUserId').mockResolvedValueOnce(provider);
+    const spy = jest.spyOn(userRepo, 'findOne').mockResolvedValueOnce(null);
+    const out = service.execute({ providerOwnerId, userToConnectId });
+    await expect(out).rejects.toThrowError(
+      new BadRequestException('User to connect not found'),
+    );
+    expect(spy).toHaveBeenCalledWith(userToConnectId);
   });
 });
