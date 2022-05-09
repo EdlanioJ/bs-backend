794212992c2fd09375c174824dc383a5534512ef
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-09 18:14:59 +0100

    test: ensure RejectConnectionService throw BadRequestException if user not found

--- src/service-provider/services/reject-connection.service.spec.ts
+++ src/service-provider/services/reject-connection.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { UserRepository } from '../../user/repositories';
 import { RequestConnectionRepository } from '../repositories';
@@ -8,6 +9,7 @@ jest.mock('../../user/repositories');
 
 describe('RejectConnectionService', () => {
   let service: RejectConnectionService;
+  let userRepo: UserRepository;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -19,9 +21,21 @@ describe('RejectConnectionService', () => {
     }).compile();
 
     service = module.get<RejectConnectionService>(RejectConnectionService);
+    userRepo = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should throw a BadRequestException if the user not found', async () => {
+    const userId = 'userId';
+    const requestId = 'requestId';
+    const spy = jest.spyOn(userRepo, 'findOne').mockResolvedValueOnce(null);
+    const out = service.execute({ userId, requestId });
+    await expect(out).rejects.toThrowError(
+      new BadRequestException('User not found'),
+    );
+    expect(spy).toHaveBeenCalledWith(userId);
   });
 });
