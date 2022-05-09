42cecc437d2a3375b290933a97d3c8bd73c2217e
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-09 09:52:57 +0100

    test: ensure SeviceController call createService with correct params

--- src/service/controllers/service.controller.spec.ts
+++ src/service/controllers/service.controller.spec.ts
@@ -10,6 +10,7 @@ jest.mock('../services');
 
 describe('ServiceController', () => {
   let controller: ServiceController;
+  let createService: CreateProviderServiceService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,9 +23,30 @@ describe('ServiceController', () => {
     }).compile();
 
     controller = module.get<ServiceController>(ServiceController);
+    createService = module.get<CreateProviderServiceService>(
+      CreateProviderServiceService,
+    );
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should call createService with correct params', async () => {
+      const spy = jest.spyOn(createService, 'execute');
+      await controller.create(
+        {
+          appointmentDurationInMinutes: 30,
+          name: 'service_name',
+        },
+        'user_id',
+      );
+      expect(spy).toHaveBeenCalledWith({
+        appointmentDurationInMinutes: 30,
+        name: 'service_name',
+        userId: 'user_id',
+      });
+    });
   });
 });
