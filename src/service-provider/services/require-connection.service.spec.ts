c399dc676854aba9b81b7df2da4306e0c10427db
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-09 22:38:23 +0100

    test: ensure RequireConnectionService throw BadRequestException if connection request already sent

--- src/service-provider/services/require-connection.service.spec.ts
+++ src/service-provider/services/require-connection.service.spec.ts
@@ -7,7 +7,11 @@ import {
   RequestConnectionRepository,
   ServiceProviderRepository,
 } from '../repositories';
-import { serviceProviderStub, userStub } from '../../../test/mocks/stubs';
+import {
+  serviceProviderStub,
+  userStub,
+  connectionRequestStub,
+} from '../../../test/mocks/stubs';
 
 jest.mock('../repositories');
 jest.mock('../../user/repositories');
@@ -17,6 +21,7 @@ describe('RequireConnectionService', () => {
   let service: RequireConnectionService;
   let providerRepo: ServiceProviderRepository;
   let userRepo: UserRepository;
+  let requestConnectionRepo: RequestConnectionRepository;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -34,6 +39,9 @@ describe('RequireConnectionService', () => {
       ServiceProviderRepository,
     );
     userRepo = module.get<UserRepository>(UserRepository);
+    requestConnectionRepo = module.get<RequestConnectionRepository>(
+      RequestConnectionRepository,
+    );
   });
 
   it('should be defined', () => {
@@ -78,5 +86,24 @@ describe('RequireConnectionService', () => {
     await expect(out).rejects.toThrowError(
       new BadRequestException('Not a valid user to connect'),
     );
+  });
+
+  it('should throw BadRequestException if connection request already sent', async () => {
+    const providerOwnerId = 'providerOwnerId';
+    const userToConnectId = 'userToConnectId';
+    const provider = serviceProviderStub();
+    const user = userStub();
+    const requestConnection = connectionRequestStub();
+    user.role = 'USER';
+    jest.spyOn(providerRepo, 'findByUserId').mockResolvedValueOnce(provider);
+    jest.spyOn(userRepo, 'findOne').mockResolvedValueOnce(user);
+    const spy = jest
+      .spyOn(requestConnectionRepo, 'findAvailable')
+      .mockResolvedValueOnce(requestConnection);
+    const out = service.execute({ providerOwnerId, userToConnectId });
+    await expect(out).rejects.toThrowError(
+      new BadRequestException('Connection request already sent'),
+    );
+    expect(spy).toHaveBeenCalledWith(provider.id, user.id);
   });
 });
