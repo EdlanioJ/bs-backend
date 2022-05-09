ce94cdbdb3f4b828a125d25dce93f5c4df34d06d
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-09 01:42:12 +0100

    test: ensure ListProviderServiceService return list of services

--- src/service/services/list-service.service.spec.ts
+++ src/service/services/list-service.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test } from '@nestjs/testing';
+import { serviceStub } from '../../../test/mocks/stubs';
 import { ServiceRepository } from '../repositories';
 import { ListProviderServiceService } from './list-service.service';
 
@@ -6,6 +7,7 @@ jest.mock('../repositories');
 
 describe('ListProviderServiceService', () => {
   let service: ListProviderServiceService;
+  let serviceRepo: ServiceRepository;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -14,9 +16,26 @@ describe('ListProviderServiceService', () => {
     service = module.get<ListProviderServiceService>(
       ListProviderServiceService,
     );
+    serviceRepo = module.get<ServiceRepository>(ServiceRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return list of services', async () => {
+    const countSpy = jest.spyOn(serviceRepo, 'count').mockResolvedValue(1);
+    const spy = jest
+      .spyOn(serviceRepo, 'findAll')
+      .mockResolvedValueOnce([serviceStub()]);
+    const result = await service.execute({ page: 1, limit: 1 });
+    expect(spy).toHaveBeenCalledWith({
+      skip: 0,
+      take: 1,
+    });
+    expect(result).toBeDefined();
+    expect(countSpy).toHaveBeenCalledTimes(1);
+    expect(result.total).toBe(1);
+    expect(result.data).toHaveLength(1);
   });
 });
