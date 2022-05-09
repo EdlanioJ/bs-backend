a227257b9fccd1990fb3c27b13969126ea1dca4a
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-09 01:28:47 +0100

    test: ensure GetProviderServiceService return a service

--- src/service/services/get-service.service.spec.ts
+++ src/service/services/get-service.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test } from '@nestjs/testing';
+import { serviceStub } from '../../../test/mocks/stubs';
 import { ServiceRepository } from '../repositories';
 import { GetProviderServiceService } from './get-service.service';
 
@@ -6,15 +7,27 @@ jest.mock('../repositories');
 
 describe('GetProviderServiceService', () => {
   let service: GetProviderServiceService;
+  let serviceRepo: ServiceRepository;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       providers: [GetProviderServiceService, ServiceRepository],
     }).compile();
     service = module.get<GetProviderServiceService>(GetProviderServiceService);
+    serviceRepo = module.get<ServiceRepository>(ServiceRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return a service', async () => {
+    const serviceId = 'serviceId';
+    const spy = jest
+      .spyOn(serviceRepo, 'findOne')
+      .mockResolvedValueOnce(serviceStub());
+    const result = await service.execute({ id: serviceId });
+    expect(spy).toHaveBeenCalledWith(serviceId);
+    expect(result).toBeDefined();
   });
 });
