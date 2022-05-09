2a8218528c8a37643a5b2c67482b494cf083d4d7
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-09 10:50:55 +0100

    test: ensure SeviceController listService return service list and total

--- src/service/controllers/service.controller.spec.ts
+++ src/service/controllers/service.controller.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { createResponse } from 'node-mocks-http';
+import { serviceStub } from '../../../test/mocks/stubs';
+import { ServiceModel } from '../models';
 import {
   CreateProviderServiceService,
   ListProviderServiceService,
@@ -11,6 +14,7 @@ jest.mock('../services');
 describe('ServiceController', () => {
   let controller: ServiceController;
   let createService: CreateProviderServiceService;
+  let listService: ListProviderServiceService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -25,6 +29,9 @@ describe('ServiceController', () => {
     controller = module.get<ServiceController>(ServiceController);
     createService = module.get<CreateProviderServiceService>(
       CreateProviderServiceService,
+    );
+    listService = module.get<ListProviderServiceService>(
+      ListProviderServiceService,
     );
   });
 
@@ -47,6 +54,33 @@ describe('ServiceController', () => {
         name: 'service_name',
         userId: 'user_id',
       });
+    });
+  });
+
+  describe('list', () => {
+    it('should call return service list and total', async () => {
+      const service = serviceStub();
+      const result = {
+        total: 1,
+        data: [ServiceModel.map(service)],
+      };
+      const spy = jest
+        .spyOn(listService, 'execute')
+        .mockResolvedValueOnce(result);
+      const res = createResponse();
+      await controller.list(1, 10, res);
+      expect(spy).toHaveBeenCalledWith({ page: 1, limit: 10 });
+      expect(res.getHeader('x-total-count')).toBe(result.total);
+      expect(res.getHeader('x-page')).toBe(1);
+      expect(res.getHeader('x-limit')).toBe(10);
+      const body = res._getJSONData();
+      expect(body[0]).toEqual(
+        expect.objectContaining({
+          id: service.id,
+          name: service.name,
+          appointmentDurationInMinutes: service.appointmentDurationInMinutes,
+        }),
+      );
     });
   });
 });
