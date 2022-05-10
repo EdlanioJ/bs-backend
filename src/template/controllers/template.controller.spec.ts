23029f41b75df0198276bb3f1fc8fc6aad173df7
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-10 16:10:11 +0100

    test: ensure TemplateController call  create template service with correct values

--- src/template/controllers/template.controller.spec.ts
+++ src/template/controllers/template.controller.spec.ts
@@ -13,6 +13,7 @@ jest.mock('../services');
 
 describe('TemplateController', () => {
   let controller: TemplateController;
+  let createTemplateService: CreateTemplateService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -26,9 +27,32 @@ describe('TemplateController', () => {
     }).compile();
 
     controller = module.get<TemplateController>(TemplateController);
+    createTemplateService = module.get<CreateTemplateService>(
+      CreateTemplateService,
+    );
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create template', () => {
+    it('should call  create template service with correct values', async () => {
+      const spy = jest.spyOn(createTemplateService, 'execute');
+      await controller.create(
+        {
+          type: 'type',
+          body: 'body',
+          subject: 'subject',
+        },
+        'userId',
+      );
+      expect(spy).toHaveBeenCalledWith({
+        type: 'type',
+        body: 'body',
+        subject: 'subject',
+        userId: 'userId',
+      });
+    });
   });
 });
