d758c377a9e12388f8cf5959a300a58050eff79c
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-10 04:18:55 +0100

    test: ensure ProviderConnectionRepository create a provider connection

--- src/service-provider/repositories/provider-connection.repository.spec.ts
+++ src/service-provider/repositories/provider-connection.repository.spec.ts
@@ -1,9 +1,11 @@
 import { Test } from '@nestjs/testing';
+import { connectionRequestStub } from '../../../test/mocks/stubs';
 import { PrismaService } from '../../prisma';
 import { ProviderConnectionRepository } from './provider-connection.repository';
 
 describe('ProviderConnectionRepository', () => {
   let repository: ProviderConnectionRepository;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -13,9 +15,22 @@ describe('ProviderConnectionRepository', () => {
     repository = module.get<ProviderConnectionRepository>(
       ProviderConnectionRepository,
     );
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(repository).toBeDefined();
+  });
+
+  it('should create a provider connection', async () => {
+    const providerConnection = connectionRequestStub();
+    prisma.providerConnection.create = jest
+      .fn()
+      .mockResolvedValueOnce(providerConnection);
+    const result = await repository.create({
+      provider: { connect: { id: 'providerId' } },
+      user: { connect: { id: 'userId' } },
+    });
+    expect(result).toBe(providerConnection);
   });
 });
