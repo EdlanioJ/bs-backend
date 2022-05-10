9c5f6661210c21e94b15eb4f90d0bd3084fa0cf0
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-10 09:54:33 +0100

    test: ensure ConnectionProviderController call reject connection service with correct values

--- src/service-provider/controllers/connection-provider.controller.spec.ts
+++ src/service-provider/controllers/connection-provider.controller.spec.ts
@@ -14,6 +14,7 @@ jest.mock('../services');
 describe('ConnectionProviderController', () => {
   let controller: ConnectionProviderController;
   let acceptConnection: AcceptConnectionService;
+  let rejectConnection: RejectConnectionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -34,6 +35,9 @@ describe('ConnectionProviderController', () => {
     acceptConnection = module.get<AcceptConnectionService>(
       AcceptConnectionService,
     );
+    rejectConnection = module.get<RejectConnectionService>(
+      RejectConnectionService,
+    );
   });
 
   it('should be defined', () => {
@@ -41,11 +45,21 @@ describe('ConnectionProviderController', () => {
   });
 
   describe('accept connection', () => {
-    it('should call accept connection with correct values', async () => {
+    it('should call accept connection service with correct values', async () => {
       const userId = 'userId';
       const requestId = 'requestId';
       const spy = jest.spyOn(acceptConnection, 'execute');
       await controller.accept(userId, requestId);
+      expect(spy).toHaveBeenCalledWith({ userId, requestId });
+    });
+  });
+
+  describe('reject connection', () => {
+    it('should call reject connection service with correct values', async () => {
+      const userId = 'userId';
+      const requestId = 'requestId';
+      const spy = jest.spyOn(rejectConnection, 'execute');
+      await controller.reject(userId, requestId);
       expect(spy).toHaveBeenCalledWith({ userId, requestId });
     });
   });
