5db44e8616cd8943f0d30ea00358be17a5a00c6a
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-10 10:44:29 +0100

    test: ensure ServiceProviderController call delete service provider service with correct values

--- src/service-provider/controllers/service-provider.controller.spec.ts
+++ src/service-provider/controllers/service-provider.controller.spec.ts
@@ -11,6 +11,7 @@ jest.mock('../services');
 describe('ServiceProviderController', () => {
   let controller: ServiceProviderController;
   let addServiceProvider: AddServiceProviderService;
+  let deleteServiceProvider: DeleteServiceProviderService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -28,6 +29,9 @@ describe('ServiceProviderController', () => {
     addServiceProvider = module.get<AddServiceProviderService>(
       AddServiceProviderService,
     );
+    deleteServiceProvider = module.get<DeleteServiceProviderService>(
+      DeleteServiceProviderService,
+    );
   });
 
   it('should be defined', () => {
@@ -41,6 +45,16 @@ describe('ServiceProviderController', () => {
       const spy = jest.spyOn(addServiceProvider, 'execute');
       await controller.add({ name }, userId);
       expect(spy).toHaveBeenCalledWith({ name, userId });
+    });
+  });
+
+  describe('delete service provider', () => {
+    it('should call delete service provider service with correct values', async () => {
+      const id = 'id';
+      const userId = 'userId';
+      const spy = jest.spyOn(deleteServiceProvider, 'execute');
+      await controller.delete(userId, id);
+      expect(spy).toHaveBeenCalledWith({ id, userId });
     });
   });
 });
