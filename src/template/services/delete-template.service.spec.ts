e264121f2853b96cd938e08c158e2c06cf62e26e
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-10 14:58:01 +0100

    test: ensure DeleteTemplateService delete a template

--- src/template/services/delete-template.service.spec.ts
+++ src/template/services/delete-template.service.spec.ts
@@ -6,6 +6,7 @@ jest.mock('../repositories');
 
 describe('DeleteTemplateService', () => {
   let service: DeleteTemplateService;
+  let templateRepo: TemplateRepository;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -13,9 +14,18 @@ describe('DeleteTemplateService', () => {
     }).compile();
 
     service = module.get<DeleteTemplateService>(DeleteTemplateService);
+    templateRepo = module.get<TemplateRepository>(TemplateRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should delete a template', async () => {
+    const id = 'any_id';
+    const spy = jest.spyOn(templateRepo, 'delete');
+    await service.execute({ id });
+    expect(spy).toHaveReturnedTimes(1);
+    expect(spy).toHaveBeenCalledWith(id);
   });
 });
