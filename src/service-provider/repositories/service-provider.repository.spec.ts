012bfb92bbb7f608f369923b293e811a1c71158b
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-10 08:33:42 +0100

    test: ensure ServiceProviderRepository create service provider

--- src/service-provider/repositories/service-provider.repository.spec.ts
+++ src/service-provider/repositories/service-provider.repository.spec.ts
@@ -1,21 +1,49 @@
 import { Test } from '@nestjs/testing';
+import { serviceProviderStub } from '../../../test/mocks/stubs';
 import { PrismaService } from '../../prisma';
 import { ServiceProviderRepository } from './service-provider.repository';
+const serviceProvider = serviceProviderStub();
+
+const db = {
+  serviceProvider: {
+    create: jest.fn().mockResolvedValue(serviceProvider),
+  },
+};
 
 describe('ServiceProviderRepository', () => {
   let repository: ServiceProviderRepository;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [ServiceProviderRepository, PrismaService],
+      providers: [
+        ServiceProviderRepository,
+        { provide: PrismaService, useValue: db },
+      ],
     }).compile();
 
     repository = module.get<ServiceProviderRepository>(
       ServiceProviderRepository,
     );
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(repository).toBeDefined();
+  });
+
+  it('should create service provider', async () => {
+    const spy = jest.spyOn(prisma.serviceProvider, 'create');
+    const result = await repository.create({
+      user: { connect: { id: 'userId' } },
+      name: 'name',
+    });
+    expect(result).toBe(serviceProvider);
+    expect(spy).toHaveBeenCalledWith({
+      data: {
+        user: { connect: { id: 'userId' } },
+        name: 'name',
+      },
+    });
   });
 });
