5b7addc8e052048690d233b648de56c658185963
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-10 14:10:02 +0100

    test: ensure CreateTemplateService throw BadRequestException if user not found

--- src/template/services/create-template.service.spec.ts
+++ src/template/services/create-template.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { UserRepository } from '../../user/repositories';
 import { TemplateRepository } from '../repositories';
@@ -8,15 +9,32 @@ jest.mock('../../user/repositories');
 
 describe('CreateTemplateService', () => {
   let service: CreateTemplateService;
+  let userRepo: UserRepository;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       providers: [CreateTemplateService, UserRepository, TemplateRepository],
     }).compile();
     service = module.get<CreateTemplateService>(CreateTemplateService);
+    userRepo = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should throw BadRequestException if user not found', async () => {
+    const spy = jest.spyOn(userRepo, 'findOne').mockReturnValue(null);
+    const out = service.execute({
+      body: 'body',
+      subject: 'subject',
+      type: 'type',
+      userId: 'userId',
+    });
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith('userId');
+    await expect(out).rejects.toThrow(
+      new BadRequestException('User not found'),
+    );
   });
 });
