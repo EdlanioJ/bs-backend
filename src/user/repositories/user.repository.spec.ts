98b9f016074dd06003f18692c3caddb4ad4095c6
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-11 15:42:43 +0100

    test: ensure UserRepository find one user by third party id

--- src/user/repositories/user.repository.spec.ts
+++ src/user/repositories/user.repository.spec.ts
@@ -76,4 +76,15 @@ describe('UserRepository', () => {
       },
     });
   });
+
+  it('should find one user by third party id', async () => {
+    const spy = jest.spyOn(prisma.user, 'findFirst');
+    const result = await repository.findOneByThirdPartyId('any_id');
+    expect(result).toEqual(user);
+    expect(spy).toHaveBeenCalledWith({
+      where: {
+        thirdPartyId: 'any_id',
+      },
+    });
+  });
 });
