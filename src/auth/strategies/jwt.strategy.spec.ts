ebb411baca5f2cf59b34acd127d7f03e9c9e2531
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-11 22:27:29 +0100

    test: ensure JwtStrategy validate and return payload

--- src/auth/strategies/jwt.strategy.spec.ts
+++ src/auth/strategies/jwt.strategy.spec.ts
@@ -19,4 +19,10 @@ describe('JwtStrategy', () => {
   it('should be defined', () => {
     expect(strategy).toBeDefined();
   });
+
+  it('should validate and return payload', async () => {
+    const payload = { sub: 'id', username: 'username', role: 'role' };
+    const out = await strategy.validate(payload);
+    expect(out).toEqual(payload);
+  });
 });
