976ead7c5b9fad450c57ecea389dc8f1c6a7ab90
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-11 10:33:40 +0100

    test: ensure RejectManagerService throw BadRequestException if no request found

--- src/user/services/reject-manager.service.spec.ts
+++ src/user/services/reject-manager.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 
 import { SendMailProducerService } from '../../mail/services';
@@ -9,6 +10,7 @@ jest.mock('../repositories');
 
 describe('RejectManagerService', () => {
   let service: RejectManagerService;
+  let managerRequestRepo: ManagerRequestRepository;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -20,9 +22,28 @@ describe('RejectManagerService', () => {
       ],
     }).compile();
     service = module.get<RejectManagerService>(RejectManagerService);
+    managerRequestRepo = module.get<ManagerRequestRepository>(
+      ManagerRequestRepository,
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should throw BadRequestException if no request found', async () => {
+    const spy = jest
+      .spyOn(managerRequestRepo, 'findAvailable')
+      .mockResolvedValue(null);
+    const out = service.execute({
+      requestId: 'requestId',
+      userId: 'userId',
+      reason: 'reason',
+    });
+    await expect(out).rejects.toThrow(
+      new BadRequestException('No request found'),
+    );
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith('requestId');
   });
 });
