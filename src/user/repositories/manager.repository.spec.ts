98e6a4aa2e780036977290dd239f1f20fd85ccd2
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-11 14:59:45 +0100

    test: ensure ManagerRepository create a manager

--- src/user/repositories/manager.repository.spec.ts
+++ src/user/repositories/manager.repository.spec.ts
@@ -2,17 +2,40 @@ import { Test } from '@nestjs/testing';
 import { PrismaService } from '../../prisma';
 import { ManagerRepository } from './manager.repository';
 
+const db = {
+  manager: {
+    create: jest.fn().mockResolvedValue(null),
+  },
+};
+
 describe('ManagerRepository', () => {
   let repository: ManagerRepository;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [ManagerRepository, PrismaService],
+      providers: [ManagerRepository, { provide: PrismaService, useValue: db }],
     }).compile();
     repository = module.get<ManagerRepository>(ManagerRepository);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(repository).toBeDefined();
+  });
+
+  it('should create a manager', async () => {
+    const spy = jest.spyOn(prisma.manager, 'create');
+    const result = await repository.create({
+      authorizedBy: { connect: { id: 'any_admin_id' } },
+      user: { connect: { id: 'any_user_id' } },
+    });
+    expect(result).toEqual(null);
+    expect(spy).toHaveBeenCalledWith({
+      data: {
+        authorizedBy: { connect: { id: 'any_admin_id' } },
+        user: { connect: { id: 'any_user_id' } },
+      },
+    });
   });
 });
