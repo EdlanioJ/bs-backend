950446ea42e32369659d95d86a80bd0148049cbb
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-12 03:42:45 +0100

    test: ensure LocalStrategy validate and return payload

--- src/auth/strategies/local.strategy.spec.ts
+++ src/auth/strategies/local.strategy.spec.ts
@@ -6,15 +6,29 @@ jest.mock('../services');
 
 describe('LocalStrategy', () => {
   let strategy: LocalStrategy;
+  let service: ValidateWithCredentialsService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       providers: [LocalStrategy, ValidateWithCredentialsService],
     }).compile();
     strategy = module.get<LocalStrategy>(LocalStrategy);
+    service = module.get<ValidateWithCredentialsService>(
+      ValidateWithCredentialsService,
+    );
   });
 
   it('should be defined', () => {
     expect(strategy).toBeDefined();
+  });
+
+  it('should validate and return payload', async () => {
+    const email = 'email';
+    const password = 'password';
+    const user = { id: 'id', username: 'username', role: 'role' };
+    const spy = jest.spyOn(service, 'execute').mockResolvedValue(user);
+    const result = await strategy.validate(email, password);
+    expect(result).toEqual({ sub: 'id', username: 'username', role: 'role' });
+    expect(spy).toHaveBeenCalledWith({ email, password });
   });
 });
