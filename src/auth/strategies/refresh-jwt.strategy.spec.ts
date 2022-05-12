449b4642c71375745326b0bae537459b9aeeee98
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-12 03:55:27 +0100

    test: ensure RefreshJwtStrategy validate and return payload

--- src/auth/strategies/refresh-jwt.strategy.spec.ts
+++ src/auth/strategies/refresh-jwt.strategy.spec.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { Test } from '@nestjs/testing';
+import { createRequest } from 'node-mocks-http';
 import { mockedConfigService } from '../../../test/mocks/services';
 import { RefreshJwtStrategy } from './refresh-jwt.strategy';
 
@@ -18,5 +19,13 @@ describe('RefreshJwtStrategy', () => {
 
   it('should be defined', () => {
     expect(strategy).toBeDefined();
+  });
+
+  it('should validate and return payload', async () => {
+    const payload = { sub: 'id', username: 'username', role: 'role' };
+    const req = createRequest();
+    req.headers.authorization = 'Bearer token';
+    const out = await strategy.validate(req, payload);
+    expect(out).toEqual({ ...payload, refreshToken: 'token' });
   });
 });
