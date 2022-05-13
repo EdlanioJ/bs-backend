6cf04267e05312149ae2d33a2b2f40edefa1fa66
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-13 01:51:48 +0100

    test: ensure RolesGuard be defined

---
+++ src/auth/guards/roles.guard.spec.ts
@@ -0,0 +1,14 @@
+import { Reflector } from '@nestjs/core';
+import { RolesGuard } from './roles.guard';
+
+describe('RolesGuard', () => {
+  let guard: RolesGuard;
+
+  beforeEach(() => {
+    guard = new RolesGuard(new Reflector());
+  });
+
+  it('should be defined', () => {
+    expect(guard).toBeDefined();
+  });
+});
