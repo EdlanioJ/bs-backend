768dcca8b59af8b651e0d6855e02dba7f982bbda
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-13 10:13:07 +0100

    test: ensure RolesGuard return false if user role not in returned roles

--- src/auth/guards/roles.guard.spec.ts
+++ src/auth/guards/roles.guard.spec.ts
@@ -1,14 +1,17 @@
 import { ExecutionContext } from '@nestjs/common';
-import { createMock } from '@golevelup/ts-jest';
+import { createMock, DeepMocked } from '@golevelup/ts-jest';
 import { Reflector } from '@nestjs/core';
 import { RolesGuard } from './roles.guard';
+import { Role } from '../entities';
 
 describe('RolesGuard', () => {
   let guard: RolesGuard;
-  const contextMock: ExecutionContext = createMock<ExecutionContext>();
+  let reflector: DeepMocked<Reflector>;
+  const contextMock = createMock<ExecutionContext>();
 
   beforeEach(() => {
-    guard = new RolesGuard(new Reflector());
+    reflector = createMock<Reflector>();
+    guard = new RolesGuard(reflector);
   });
 
   it('should be defined', () => {
@@ -16,9 +19,22 @@ describe('RolesGuard', () => {
   });
 
   describe('canActivate', () => {
-    it('should return true if no roles returned', () => {
-      const result = guard.canActivate(contextMock);
-      expect(result).toBe(true);
+    it('should return true if no roles returned', async () => {
+      reflector.getAllAndOverride.mockReturnValue(null);
+      const result = await guard.canActivate(contextMock);
+      expect(result).toBeTruthy();
+    });
+
+    it('should return false if user role not in returned roles', async () => {
+      const roles: Role[] = [Role.USER, Role.ADMIN];
+      contextMock.switchToHttp().getRequest.mockReturnValue({
+        user: {
+          role: Role.MANAGER,
+        },
+      });
+      reflector.getAllAndOverride.mockReturnValue(roles);
+      const result = await guard.canActivate(contextMock);
+      expect(result).toBeFalsy();
     });
   });
 });
