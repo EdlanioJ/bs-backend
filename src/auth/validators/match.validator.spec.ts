016fb8b873b02856a2f9286ea65903962933a3e7
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-13 11:08:38 +0100

    test: ensure Match validator return error

--- src/auth/validators/match.validator.spec.ts
+++ src/auth/validators/match.validator.spec.ts
@@ -16,4 +16,21 @@ describe('Match', () => {
     const errors = await validate(dto);
     expect(errors).toEqual([]);
   });
+
+  it('should return error', async () => {
+    const dto = new TestDto();
+    dto.password = '123456';
+    dto.confirmPassword = '1234567';
+    const errors = await validate(dto);
+    console.log(errors);
+    expect(errors).toHaveLength(1);
+    expect(errors[0]).toEqual(
+      expect.objectContaining({
+        property: 'confirmPassword',
+        constraints: {
+          Match: 'Password does not match',
+        },
+      }),
+    );
+  });
 });
