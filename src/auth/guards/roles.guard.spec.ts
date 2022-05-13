875170bdb70503b1cc4cba61644f17849f6b563c
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-13 08:13:35 +0100

    test: ensure RolesGuard return true if no roles returned

--- src/auth/guards/roles.guard.spec.ts
+++ src/auth/guards/roles.guard.spec.ts
@@ -1,8 +1,11 @@
+import { ExecutionContext } from '@nestjs/common';
+import { createMock } from '@golevelup/ts-jest';
 import { Reflector } from '@nestjs/core';
 import { RolesGuard } from './roles.guard';
 
 describe('RolesGuard', () => {
   let guard: RolesGuard;
+  const contextMock: ExecutionContext = createMock<ExecutionContext>();
 
   beforeEach(() => {
     guard = new RolesGuard(new Reflector());
@@ -10,5 +13,12 @@ describe('RolesGuard', () => {
 
   it('should be defined', () => {
     expect(guard).toBeDefined();
+  });
+
+  describe('canActivate', () => {
+    it('should return true if no roles returned', () => {
+      const result = guard.canActivate(contextMock);
+      expect(result).toBe(true);
+    });
   });
 });
