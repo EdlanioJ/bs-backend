c31e5307b5d846a234bcea768cd7594ee71a8331
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-19 12:53:21 +0100

    test: ensure AppointmentController (e2e) /appointment/employee/:id (GET) return 200 status code

--- test/appointment.e2e-spec.ts
+++ test/appointment.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker';
-import { addMinutes } from 'date-fns';
+import { addDays, addMinutes } from 'date-fns';
 import * as request from 'supertest';
 import * as jwt from 'jsonwebtoken';
 import { INestApplication } from '@nestjs/common';
@@ -221,6 +221,74 @@ describe('AppointmentController (e2e)', () => {
 
     const response = await request(app.getHttpServer())
       .get('/appointment/')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0].id).toBe(appointment.id);
+    expect(response.headers['x-total-count']).toBe('1');
+  });
+
+  it('/appointment/employee/:id (GET)', async () => {
+    const manager = await prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        name: faker.name.findName(),
+        role: 'MANAGER',
+      },
+    });
+    const provider = await prisma.serviceProvider.create({
+      data: {
+        name: faker.company.companyName(),
+        user: { connect: { id: manager.id } },
+      },
+    });
+
+    const service = await prisma.service.create({
+      data: {
+        name: 'My Service',
+        appointmentDurationInMinutes: 30,
+        provider: { connect: { id: provider.id } },
+      },
+    });
+
+    const employee = await prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        name: faker.name.findName(),
+        role: 'EMPLOYEE',
+      },
+    });
+
+    const customer = await prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        name: faker.name.findName(),
+      },
+    });
+
+    const startAt = addDays(new Date(), 4);
+    const appointment = await prisma.appointment.create({
+      data: {
+        service: { connect: { id: service.id } },
+        employee: { connect: { id: employee.id } },
+        customer: { connect: { id: customer.id } },
+        start: startAt,
+        end: addMinutes(startAt, service.appointmentDurationInMinutes),
+      },
+    });
+
+    const token = jwt.sign(
+      { sub: employee.id, role: employee.role, email: employee.email },
+      configService.get('JWT_SECRET'),
+      { expiresIn: '15m' },
+    );
+
+    const response = await request(app.getHttpServer())
+      .get(`/appointment/employee/${employee.id}`)
       .set('Authorization', `Bearer ${token}`);
 
     expect(response.status).toBe(200);
