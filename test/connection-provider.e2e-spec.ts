04ade47ed972bf28d961e8cc8ff5f7d323f28cf9
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-26 11:30:53 +0100

    test: ConnectionProviderController (e2e) /provider/connection/require/:userId (POST) return created status code

---
+++ test/connection-provider.e2e-spec.ts
@@ -0,0 +1,73 @@
+import faker from '@faker-js/faker';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { AuthHelpers } from '../src/auth/helpers';
+import { PrismaService } from '../src/prisma';
+
+jest.setTimeout(30000);
+
+describe('ConnectionProviderController (e2e)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let authHelpers: AuthHelpers;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+    prisma = app.get(PrismaService);
+    authHelpers = app.get(AuthHelpers);
+  });
+
+  afterEach(async () => {
+    await prisma.providerConnectionRequest.deleteMany();
+    await prisma.serviceProvider.deleteMany();
+    await prisma.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+    await app.close();
+  });
+
+  it('/provider/connection/require/:userId (POST)', async () => {
+    const manager = await prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+        role: 'MANAGER',
+        name: faker.name.findName(),
+      },
+    });
+
+    await prisma.serviceProvider.create({
+      data: {
+        name: faker.company.companyName(),
+        user: { connect: { id: manager.id } },
+      },
+    });
+
+    const user = await prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+        name: faker.name.findName(),
+      },
+    });
+
+    const { accessToken } = await authHelpers.generateTokens(
+      manager.id,
+      manager.name,
+      manager.role,
+    );
+
+    const response = await request(app.getHttpServer())
+      .post(`/provider/connection/require/${user.id}`)
+      .set('Authorization', `Bearer ${accessToken}`);
+
+    expect(response.status).toBe(HttpStatus.CREATED);
+  });
+});
