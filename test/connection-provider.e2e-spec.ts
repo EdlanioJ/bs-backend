65703e5550476df522ddb248cbfda19e1b49af03
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-26 12:43:51 +0100

    test: ConnectionProviderController (e2e) /provider/connection/reject/:id (POST) return no content status code

--- test/connection-provider.e2e-spec.ts
+++ test/connection-provider.e2e-spec.ts
@@ -115,4 +115,48 @@ describe('ConnectionProviderController (e2e)', () => {
     await prisma.providerConnection.deleteMany();
     expect(response.status).toBe(HttpStatus.CREATED);
   });
+
+  it('/provider/connection/reject/:id', async () => {
+    const manager = await prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+        role: 'MANAGER',
+        name: faker.name.findName(),
+      },
+    });
+
+    const provider = await prisma.serviceProvider.create({
+      data: {
+        name: faker.company.companyName(),
+        user: { connect: { id: manager.id } },
+      },
+    });
+
+    const user = await prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+        name: faker.name.findName(),
+      },
+    });
+
+    const connectionRequest = await prisma.providerConnectionRequest.create({
+      data: {
+        employee: { connect: { id: user.id } },
+        provider: { connect: { id: provider.id } },
+        status: 'PENDING',
+      },
+    });
+
+    const { accessToken } = await authHelpers.generateTokens(
+      user.id,
+      user.name,
+      user.role,
+    );
+
+    const response = await request(app.getHttpServer())
+      .patch(`/provider/connection/reject/${connectionRequest.id}`)
+      .set('Authorization', `Bearer ${accessToken}`);
+
+    expect(response.status).toBe(HttpStatus.NO_CONTENT);
+  });
 });
