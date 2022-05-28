f555b0ea9374b59bd4da64821d00dedca5e91cbb
Edlâneo Júlio <edlanioj@gmail.com>
2022-05-28 14:07:18 +0100

    test: UserManagerController (e2e) /user/manager/request/ (POST) return created status code

---
+++ test/user-manager.e2e-spec.ts
@@ -0,0 +1,59 @@
+import { faker } from '@faker-js/faker';
+import * as request from 'supertest';
+
+import { INestApplication, HttpStatus } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+import { AuthHelpers } from '../src/auth/helpers';
+import { PrismaService } from '../src/prisma';
+
+jest.setTimeout(30000);
+
+describe('UserManagerController (e2e)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let authHelpers: AuthHelpers;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+    prisma = app.get(PrismaService);
+    authHelpers = app.get(AuthHelpers);
+  });
+
+  afterEach(async () => {
+    await prisma.managerRequest.deleteMany({});
+    await prisma.user.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+    await app.close();
+  });
+
+  it('/user/manager/request/ (POST)', async () => {
+    const user = await prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+        name: faker.name.findName(),
+        role: 'USER',
+      },
+    });
+
+    const { accessToken } = await authHelpers.generateTokens(
+      user.id,
+      user.name,
+      user.role,
+    );
+
+    const response = await request(app.getHttpServer())
+      .post('/user/manager/request')
+      .set('Authorization', `Bearer ${accessToken}`);
+
+    expect(response.status).toBe(HttpStatus.CREATED);
+  });
+});
