55ad913a59fb830ec34aac07e9d812d141639d16
Edlâneo Júlio <edlanioj@gmail.com>
2022-06-25 22:55:04 +0100

    test: ensure UserController profile method return a user model

--- src/user/controllers/user.controller.spec.ts
+++ src/user/controllers/user.controller.spec.ts
@@ -6,6 +6,7 @@ jest.mock('../services');
 
 describe('UserController', () => {
   let controller: UserController;
+  let getUserService: GetUserService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -14,9 +15,33 @@ describe('UserController', () => {
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    getUserService = module.get<GetUserService>(GetUserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('profile', () => {
+    it('should return a user model', async () => {
+      const id = 'any_id';
+      const user = {
+        id,
+        name: 'any_name',
+        avatar: 'any_avatar',
+        createdAt: new Date(),
+      };
+      const spy = jest.spyOn(getUserService, 'execute').mockResolvedValue(user);
+      const out = await controller.profile(id);
+      expect(spy).toHaveBeenCalledWith({ id });
+      expect(out).toEqual(
+        expect.objectContaining({
+          id,
+          name: user.name,
+          avatar: user.avatar,
+          createdAt: user.createdAt,
+        }),
+      );
+    });
   });
 });
