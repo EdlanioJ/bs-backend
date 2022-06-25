c3434fd93f2016e7807d68689b99963a71374f09
Edlâneo Júlio <edlanioj@gmail.com>
2022-06-25 17:07:49 +0100

    test: ensure GetUserService execute return a user model

--- src/user/services/get-user.service.spec.ts
+++ src/user/services/get-user.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test } from '@nestjs/testing';
+import { userStub } from '../../../test/stubs';
 import { UserRepository } from '../repositories';
 import { GetUserService } from './get-user.service';
 
@@ -6,6 +7,7 @@ jest.mock('../repositories');
 
 describe('GetUserService', () => {
   let service: GetUserService;
+  let userRepo: UserRepository;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -13,9 +15,26 @@ describe('GetUserService', () => {
     }).compile();
 
     service = module.get<GetUserService>(GetUserService);
+    userRepo = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return a user model', async () => {
+    const id = 'any_id';
+    const user = userStub();
+    const spy = jest.spyOn(userRepo, 'findOne').mockResolvedValue(user);
+    const out = await service.execute({ id });
+    expect(spy).toHaveBeenCalledWith(id);
+    expect(out).toEqual(
+      expect.objectContaining({
+        id: user.id,
+        name: user.name,
+        avatar: user.avatar,
+        createdAt: user.createdAt,
+      }),
+    );
   });
 });
